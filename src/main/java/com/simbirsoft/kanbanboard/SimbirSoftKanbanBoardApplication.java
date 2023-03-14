e42a96afb8cc37282695c25422f2ca8bb0defe4f
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-14 11:53:13 +0300

    Initial commit

---
+++ src/main/java/com/simbirsoft/kanbanboard/SimbirSoftKanbanBoardApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftKanbanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirSoftKanbanBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simbirsoft/kanbanboard/SimbirSoftKanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoftKanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
