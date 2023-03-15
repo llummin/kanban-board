99f09e1f8f27859a58e1c6a07fd7907209158f9d
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-15 22:45:46 +0300

    добавлен класс модели роли

---
+++ src/main/java/models/Role.java
@@ -0,0 +1,51 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "role_id")
+  private Long id;
+
+  @Column(name = "role_name", unique = true, nullable = false)
+  private String name;
+
+  @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+  private Set<User> users = new HashSet<>();
+
+  public Role() {
+  }
+
+  public Role(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<User> getUsers() {
+    return users;
+  }
+
+  public void setUsers(Set<User> users) {
+    this.users = users;
+  }
+}
+
