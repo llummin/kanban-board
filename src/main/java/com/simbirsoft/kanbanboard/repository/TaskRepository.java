bcf72b6e24f80629a62c6e6855d2a4991c9a22e2
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-16 01:52:00 +0300

    добавлен репозиторий для задачи

--- src/main/java/com/simbirsoft/kanbanboard/repository/TaskRepository.java
+++ src/main/java/com/simbirsoft/kanbanboard/repository/TaskRepository.java
@@ -1,10 +1,10 @@
 package com.simbirsoft.kanbanboard.repository;
 
-import com.simbirsoft.kanbanboard.model.Project;
+import com.simbirsoft.kanbanboard.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
