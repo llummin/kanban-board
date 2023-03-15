02db3f0321c5855ea0b311c28f478678e03f714e
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-16 00:44:43 +0300

    переход класса Role на record

--- src/main/java/com/simbirsoft/kanbanboard/model/Role.java
+++ src/main/java/com/simbirsoft/kanbanboard/model/Role.java
@@ -1,52 +1,22 @@
 package com.simbirsoft.kanbanboard.model;
 
-import javax.persistence.*;
 import java.util.Set;
-import java.util.HashSet;
+import javax.persistence.*;
+
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public record Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    Long id,
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "role_id")
-  private Long id;
+    @Column(name = "role_name", unique = true, nullable = false)
+    String name,
 
-  @Column(name = "role_name", unique = true, nullable = false)
-  private String name;
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    Set<User> users
+) {
 
-  @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
-  private Set<User> users = new HashSet<>();
-
-  public Role() {
-  }
-
-  public Role(String name) {
-    this.name = name;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Set<User> getUsers() {
-    return users;
-  }
-
-  public void setUsers(Set<User> users) {
-    this.users = users;
-  }
 }
