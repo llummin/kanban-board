d7d0537aaf762892123be77a0d996083f7bd9104
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-15 23:57:23 +0300

    переход класса Project на record

--- src/main/java/com/simbirsoft/kanbanboard/model/Project.java
+++ src/main/java/com/simbirsoft/kanbanboard/model/Project.java
@@ -6,82 +6,26 @@ import java.util.List;
 
 @Entity
 @Table(name = "projects")
-public class Project {
+public record Project(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "proj_id")
+    Long id,
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "proj_id")
-  private Long id;
+    @Column(name = "proj_name", nullable = false)
+    String name,
 
-  @Column(name = "proj_name", nullable = false)
-  private String name;
+    @Column(name = "proj_start_date", nullable = false)
+    LocalDateTime startDate,
 
-  @Column(name = "proj_start_date", nullable = false)
-  private LocalDateTime startDate;
+    @Column(name = "proj_end_date")
+    LocalDateTime endDate,
 
-  @Column(name = "proj_end_date")
-  private LocalDateTime endDate;
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    List<Task> tasks,
 
-  @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
-  private List<Task> tasks;
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    List<Release> releases
+) {
 
-  @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
-  private List<Release> releases;
-
-  public Project() {
-  }
-
-  public Project(String name, LocalDateTime startDate, LocalDateTime endDate) {
-    this.name = name;
-    this.startDate = startDate;
-    this.endDate = endDate;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public LocalDateTime getStartDate() {
-    return startDate;
-  }
-
-  public void setStartDate(LocalDateTime startDate) {
-    this.startDate = startDate;
-  }
-
-  public LocalDateTime getEndDate() {
-    return endDate;
-  }
-
-  public void setEndDate(LocalDateTime endDate) {
-    this.endDate = endDate;
-  }
-
-  public List<Task> getTasks() {
-    return tasks;
-  }
-
-  public void setTasks(List<Task> tasks) {
-    this.tasks = tasks;
-  }
-
-  public List<Release> getReleases() {
-    return releases;
-  }
-
-  public void setReleases(List<Release> releases) {
-    this.releases = releases;
-  }
 }
