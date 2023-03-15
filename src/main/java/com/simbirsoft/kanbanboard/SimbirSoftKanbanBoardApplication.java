5237fd9544e539710cef1536f6e1b7a20041e8db
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-15 15:56:45 +0300

    добавлено подключение к базе данных

--- src/main/java/com/simbirsoft/kanbanboard/SimbirSoftKanbanBoardApplication.java
+++ src/main/java/com/simbirsoft/kanbanboard/SimbirSoftKanbanBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimbirSoftKanbanBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimbirSoftKanbanBoardApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SimbirSoftKanbanBoardApplication.class, args);
+  }
 
 }
