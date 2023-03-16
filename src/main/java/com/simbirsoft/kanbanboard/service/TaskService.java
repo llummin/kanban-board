d56ead9f608c0753bed2bd0ef05c8f566c006906
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-16 17:33:49 +0300

    добавлен сервис для задачи

---
+++ src/main/java/com/simbirsoft/kanbanboard/service/TaskService.java
@@ -0,0 +1,34 @@
+package com.simbirsoft.kanbanboard.service;
+
+import com.simbirsoft.kanbanboard.model.Task;
+import com.simbirsoft.kanbanboard.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+  private final TaskRepository taskRepository;
+
+  public TaskService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  public List<Task> getAllTasks() {
+    return taskRepository.findAll();
+  }
+
+  public Optional<Task> getTaskById(Long id) {
+    return taskRepository.findById(id);
+  }
+
+  public Task saveTask(Task task) {
+    return taskRepository.save(task);
+  }
+
+  public void deleteTaskById(Long id) {
+    taskRepository.deleteById(id);
+  }
+}
