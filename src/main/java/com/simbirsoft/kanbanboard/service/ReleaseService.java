3529184d92be99d929ba49fe04f7b0145e8f0dbe
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-16 17:26:00 +0300

    добавлен сервис для релиза

---
+++ src/main/java/com/simbirsoft/kanbanboard/service/ReleaseService.java
@@ -0,0 +1,34 @@
+package com.simbirsoft.kanbanboard.service;
+
+import com.simbirsoft.kanbanboard.model.Release;
+import com.simbirsoft.kanbanboard.repository.ReleaseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReleaseService {
+
+  private final ReleaseRepository releaseRepository;
+
+  public ReleaseService(ReleaseRepository releaseRepository) {
+    this.releaseRepository = releaseRepository;
+  }
+
+  public List<Release> getAllReleases() {
+    return releaseRepository.findAll();
+  }
+
+  public Optional<Release> getReleaseById(Long id) {
+    return releaseRepository.findById(id);
+  }
+
+  public Release saveRelease(Release release) {
+    return releaseRepository.save(release);
+  }
+
+  public void deleteReleaseById(Long id) {
+    releaseRepository.deleteById(id);
+  }
+}
