d6ff4e04febd5b6e202d53fe1b7f5f6798bb203e
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-17 02:23:07 +0300

    просмотр отдельного проекта

--- src/main/java/com/simbirsoft/kanbanboard/controller/ProjectController.java
+++ src/main/java/com/simbirsoft/kanbanboard/controller/ProjectController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +24,12 @@ public class ProjectController {
     List<Project> projects = projectService.getAllProjects();
     model.addAttribute("projects", projects);
     return "projects";
+  }
+
+  @GetMapping("/{id}")
+  public String showProjectById(@PathVariable Long id, Model model) {
+    model.addAttribute("project", projectService.getProjectById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid project Id:" + id)));
+    return "project";
   }
 }
