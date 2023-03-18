d4da00d9aff546464f7379b8c01a45ed7a9c8858
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-18 23:29:27 +0300

    изменение структуры базы данных

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,41 +1,56 @@
+-- Создание таблицы проектов
 CREATE TABLE projects
 (
-    proj_id         SERIAL PRIMARY KEY,
-    proj_name       VARCHAR(255) NOT NULL,
-    proj_start_date TIMESTAMP    NOT NULL,
-    proj_end_date   TIMESTAMP
+    proj_id      SERIAL PRIMARY KEY,
+    proj_title   VARCHAR(255) NOT NULL,
+    proj_is_open BOOLEAN      NOT NULL DEFAULT TRUE
 );
 
+-- Создание таблицы задач
 CREATE TABLE tasks
 (
-    task_id              SERIAL PRIMARY KEY,
-    project_id           INTEGER      NOT NULL REFERENCES projects (proj_id) ON DELETE CASCADE,
-    task_name            VARCHAR(255) NOT NULL,
-    task_author          VARCHAR(255) NOT NULL,
-    task_performer       VARCHAR(255) NOT NULL,
-    task_release_version VARCHAR(255) NOT NULL,
-    task_status          VARCHAR(255) NOT NULL
+    task_id        SERIAL PRIMARY KEY,
+    proj_id        INTEGER      NOT NULL,
+    task_name      VARCHAR(255) NOT NULL,
+    task_author    VARCHAR(255) NOT NULL,
+    task_performer VARCHAR(255) NULL,
+    task_status    VARCHAR(50)  NOT NULL CHECK (task_status IN ('BACKLOG', 'IN_PROGRESS', 'DONE')),
+    FOREIGN KEY (proj_id)
+        REFERENCES projects (proj_id)
+        ON DELETE CASCADE
 );
 
+-- Создание таблицы релизов
 CREATE TABLE releases
 (
     rls_id         SERIAL PRIMARY KEY,
-    project_id     INTEGER   NOT NULL REFERENCES projects (proj_id) ON DELETE CASCADE,
-    rls_start_date TIMESTAMP NOT NULL,
-    rls_end_date   TIMESTAMP NOT NULL
+    task_id        INTEGER     NOT NULL,
+    rls_version    VARCHAR(50) NOT NULL,
+    rls_start_date TIMESTAMP   NOT NULL,
+    rls_end_date   TIMESTAMP   NULL,
+    FOREIGN KEY (task_id)
+        REFERENCES tasks (task_id)
+        ON DELETE CASCADE
 );
 
+-- Создание таблицы ролей
 CREATE TABLE roles
 (
     role_id   SERIAL PRIMARY KEY,
-    role_name VARCHAR(50) NOT NULL UNIQUE
+    role_name VARCHAR(50) NOT NULL
 );
 
+-- Создание таблицы пользователей
 CREATE TABLE users
 (
     user_id       SERIAL PRIMARY KEY,
-    role_id       INTEGER      NOT NULL REFERENCES roles (role_id),
-    user_username VARCHAR(255) NOT NULL UNIQUE,
+    role_id       INTEGER      NOT NULL,
+    user_name     VARCHAR(255) NOT NULL UNIQUE,
     user_password VARCHAR(255) NOT NULL,
-    user_email    VARCHAR(50)  NOT NULL UNIQUE
+    FOREIGN KEY (role_id)
+        REFERENCES roles (role_id)
 );
+
+-- Создание индексов
+CREATE INDEX tasks_proj_id_index ON tasks (proj_id);
+CREATE INDEX releases_task_id_index ON releases (task_id);
