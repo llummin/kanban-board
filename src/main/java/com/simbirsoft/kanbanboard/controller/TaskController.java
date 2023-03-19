51d83f860d9992426ba5cdb4531f252806d7360e
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-19 13:19:37 +0300

    отображение задач с релизами

--- src/main/java/com/simbirsoft/kanbanboard/controller/TaskController.java
+++ src/main/java/com/simbirsoft/kanbanboard/controller/TaskController.java
@@ -1,18 +1,20 @@
 package com.simbirsoft.kanbanboard.controller;
 
 import com.simbirsoft.kanbanboard.model.Project;
+import com.simbirsoft.kanbanboard.model.Release;
 import com.simbirsoft.kanbanboard.model.Task;
 import com.simbirsoft.kanbanboard.service.ProjectService;
 import com.simbirsoft.kanbanboard.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TaskController {
-
   private final TaskService taskService;
   private final ProjectService projectService;
 
@@ -22,12 +24,11 @@ public class TaskController {
   }
 
   @GetMapping("/projects/{id}")
-  public String showTasksByProjectId(@PathVariable Long id, Model model) {
-    Project project = projectService.getProjectById(id)
-        .orElseThrow(() -> new IllegalArgumentException("Недопустимый id проекта:" + id));
-    List<Task> tasks = taskService.getTasksByProject(project);
+  public String viewTasksByProject(@PathVariable("id") Long projectId, Model model) {
+    Project project = projectService.getProjectById(projectId)
+        .orElseThrow(() -> new IllegalArgumentException("Недопустимый id проекта:" + projectId));
     model.addAttribute("project", project);
-    model.addAttribute("tasks", tasks);
+    model.addAttribute("tasks", taskService.getTasksByProject(project));
     return "task/index";
   }
 }
